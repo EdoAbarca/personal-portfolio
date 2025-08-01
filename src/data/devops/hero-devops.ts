bffbfe2a7f4939d93764e5425d5adbc5997da3ae
EdoAbarca <eduardo.abarca.c@usach.cl>
2025-07-31 20:40:11 -0400

    feat: Información de perfil devops creada en archivos .ts

---
+++ src/data/devops/hero-devops.ts
@@ -0,0 +1,10 @@
+export const devopsHero ={
+    role: "DevOps Trainee",
+    introduction: "Trainee DevOps transitioning from FullStack Web Development with focus on process automation, software portability and AWS deployments, joining development and operations principles to optimize workflows. My background includes technical knowledge in scripting, Linux systems, CI/CD pipelines, containerization and orchestration, Infrastructure as Code, cloud computing, monitoring, and observability.",
+    metrics: [
+        { label: "Current DevOps experience", value: "1+ year" },
+        { label: "Current formal DevOps experience", value: "Entry level" },
+        { label: "Projects Completed", value: "10+" }
+    ],
+    linktoCV: "/EduardoAbarca_CVDevOps.pdf",
+}
---
+++ src/data/devops/projects-devops.ts
@@ -0,0 +1,26 @@
+export const devopsProjectList = [
+    {
+      name: "PoC AI Detector",
+      img: "/fullstack/poc.png",
+      description: "An application to detect AI-generated text in file-based evaluation-focused documents. I used Docker and Kubernetes to containerize and orchestrate the application, ensuring scalability, reliability and a <1 minute initialization time.",
+      tech: ["Docker", "Kubernetes"],
+      githubUrl: "#",
+      demoUrl: "#"
+    },
+    {
+      name: "Rotativa Myra",
+      img: "/fullstack/myra.png",
+      description: "A software to calculate employees payments in MyraSalud company, fixed to unattended shifts, holidays, extra hours and licenses. I deployed it using DigitalOcean App Platform, ensuring a reliable and scalable deployment.",
+      tech: ["DigitalOcean"],
+      githubUrl: "#",
+      demoUrl: "#"
+    },
+    {
+      name: "HealthTrack",
+      img: "/devops/red_cross.jpg",
+      description: "A software to track user's weight through uploads. I used Github Actions for CI/CD, ensuring automated testing and deployment processes in under 1 minute and 30 seconds among unit, performance, static analysis and code coverage analysis tests.",
+      tech: ["GitHub Actions", "Spring Boot", "SonarQube", "JUnit", "JMeter", "JaCoCO"],
+      githubUrl: "https://github.com/EdoAbarca/healthtrack",
+      demoUrl: "#"
+    },
+  ];
